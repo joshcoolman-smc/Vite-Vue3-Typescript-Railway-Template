85b5bd4dd1063de3574c073b365832fc82592bcb
Josh C, UX Developer <josh.coolman@singlemindconsulting.com>
2023-01-03 21:15:16 -0800

    add tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,52 +1,21 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { storeToRefs } from 'pinia';
+import { useStateStore } from '../store/state';
 
-defineProps<{ msg: string }>()
+defineProps<{ msg?: string }>()
 
-const count = ref(0)
+const store = useStateStore()
+const { name, doubleCount } = storeToRefs(store);
+const { increment } = store;
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div class="bg-slate-200 p-5">
+    <h1 class="text-4xl font-bold text-slate-700 mb-5">Hello {{ name }} {{ doubleCount }}</h1>
+    <button @click="increment" class="btn-primary">Count</button>
+  </div>
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
-}
 
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import './styles/app.css';
+
+const pinia = createPinia()
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+
+createApp(App).use(pinia).mount('#app')
